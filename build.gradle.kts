bcfcba7b96a425b608ccc9d6150f8ec8f2dad06d
dtorannpu <dtorannpu@gmail.com>
2025-05-25 14:55:12 +0900

    バージョン管理に追加

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("com.diffplug.spotless") version "7.0.3"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+spotless {
+	java {
+		lineEndings = com.diffplug.spotless.LineEnding.UNIX
+		importOrder()
+		removeUnusedImports()
+		cleanthat()
+			.sourceCompatibility("21")
+		googleJavaFormat()
+		formatAnnotations()
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "axon-server-example"
