eb507ace1a4362f7360ebbeb36a6b0d305ca8d90
dtorannpu <dtorannpu@gmail.com>
2026-01-24 13:43:16 +0900

    Spotlessを8.2.0に更新、クエリサービスのコードをフォーマット

--- src/main/java/com/example/query/service/SampleQueryService.java
+++ src/main/java/com/example/query/service/SampleQueryService.java
@@ -5,7 +5,6 @@ import com.example.query.dto.SampleListDTO;
 import com.example.query.request.SampleFindAllQuery;
 import com.example.query.request.SampleQuery;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.messaging.queryhandling.gateway.QueryGateway;
 import org.springframework.stereotype.Service;
@@ -17,14 +16,10 @@ public class SampleQueryService {
   private final QueryGateway queryGateway;
 
   public Optional<SampleDTO> get(Long id) {
-    return Optional.ofNullable(queryGateway
-        .query(new SampleQuery(id), SampleDTO.class)
-        .join());
+    return Optional.ofNullable(queryGateway.query(new SampleQuery(id), SampleDTO.class).join());
   }
 
   public SampleListDTO findAll() {
-    return queryGateway
-        .query(new SampleFindAllQuery(), SampleListDTO.class)
-        .join();
+    return queryGateway.query(new SampleFindAllQuery(), SampleListDTO.class).join();
   }
 }
