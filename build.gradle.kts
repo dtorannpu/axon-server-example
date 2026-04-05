373f8ca4de9cb01ee02bda7378440922f3524bdb
dtorannpu <dtorannpu@gmail.com>
2026-04-05 10:46:37 +0900

    Spring BootとSpotlessプラグインのバージョンを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.2"
+    id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "8.2.1"
+    id("com.diffplug.spotless") version "8.4.0"
 }
 
 group = "com.example"
